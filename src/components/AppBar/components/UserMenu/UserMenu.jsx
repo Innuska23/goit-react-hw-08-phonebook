7901e6d4bd52fab04c065b81449bde773955c791
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-25 15:42:22 +0300

    Update UserMenu

--- src/components/AppBar/components/UserMenu/UserMenu.jsx
+++ src/components/AppBar/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import useAuth from '../../../hooks/UseAuth';
@@ -7,6 +8,12 @@ import { logOut } from '../../../redux/auth/AuthOperation';
 export default function UserMenu() {
     const dispatch = useDispatch();
     const { user } = useAuth();
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleLogoutClick = () => {
+        setIsLoading(true)
+        dispatch(logOut()).unwrap().finally(() => setIsLoading(false))
+    }
 
     return (
         <>
@@ -14,7 +21,12 @@ export default function UserMenu() {
                 <Text>Hi, {user.name}!</Text>
                 <Text>Email: {user.email}</Text>
             </Box>
-            <Button type="button" colorScheme='gray' onClick={() => dispatch(logOut())}>
+            <Button
+                type="button"
+                colorScheme='gray'
+                onClick={handleLogoutClick}
+                disabled={isLoading}
+                isLoading={isLoading}>
                 Logout
             </Button>
         </>

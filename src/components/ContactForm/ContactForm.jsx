3e3530c4d9d6101dd6a76f91bd41dc50eb6cbedf
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-25 22:48:39 +0300

    fixes button Add contact

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,25 +2,12 @@ import { nanoid } from '@reduxjs/toolkit';
 import { Form, Formik } from 'formik';
 import * as yup from 'yup';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Flex, Spinner, Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 
 import { addContact } from '../redux/operations';
 import { selectContacts } from '../redux/selectors';
 import { InputField } from 'components/InputField';
-
-const ContactSchema = yup.object().shape({
-  name: yup
-    .string()
-    .trim()
-    .required()
-    .matches(/^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$/g, 'Should be name'),
-  number: yup
-    .string()
-    .trim()
-    .required()
-    .matches(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g, 'Should be phone number'),
-
-});
+import { useMemo } from 'react';
 
 const initialValues = {
   name: '',
@@ -31,12 +18,30 @@ const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
 
-  const handleSubmit = async ({ name, number }, actions) => {
-    if (contacts.find(existContact => existContact.name.toLowerCase() === name.toLowerCase())) {
-      await actions.setFieldError('name', `${name} is already in contacts`)
-      return;
-    }
+  const validation = useMemo(() => yup.object().shape({
+    name: yup
+      .string()
+      .matches(/^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$/g, 'Should be name')
+      .test(
+        '',
+        ({ value }) => `${value} is already in contacts`,
+        (value) => {
+          return !contacts.find(existContact => existContact.name.toLowerCase() === value.toLowerCase())
+        })
+      .required(),
+    number: yup
+      .string()
+      .matches(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g, 'Should be phone number')
+      .test('',
+        ({ value }) => `${value} is already in contacts`,
+        (value) => {
+          return !contacts.find(existContact => existContact.number.toLowerCase() === value.toLowerCase())
+        })
+      .required(),
 
+  }), [contacts])
+
+  const handleSubmit = async ({ name, number }, actions) => {
     const newContact = {
       id: nanoid(),
       name,
@@ -51,14 +56,14 @@ const ContactForm = () => {
       <Formik
         onSubmit={handleSubmit}
         initialValues={initialValues}
-        validationSchema={ContactSchema}
-        autoComplete="off"
+        validationSchema={validation}
       >
         {({ isSubmitting, isValidating }) => (<Form>
           <Flex width='600px' flexDirection='column' gap={6}>
             <Text fontSize='2xl'>New phone</Text>
 
             <InputField
+              id="name"
               label="Name"
               placeholder="Please enter your name"
               type="name"
@@ -66,14 +71,19 @@ const ContactForm = () => {
             />
 
             <InputField
+              id="number"
               label="Number"
               placeholder="Please enter your number"
               type="tel"
               name="number"
             />
 
-            <Button width='xs' type='summit' disabled={isSubmitting}>
-              {isSubmitting || isValidating ? <Spinner /> : <Text>Add new contact</Text>}
+            <Button
+              width='xs'
+              type="submit"
+              disabled={isSubmitting}
+              isLoading={isSubmitting}>
+              <Text>Add new contact</Text>
             </Button>
           </Flex>
         </Form>)}
